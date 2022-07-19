23caef7e9418d1939b39bb9c69d6262905f62229
FXDarkLight <andre2262911@gmail.com>
2022-07-19 10:23:27 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router} from "react-router-dom"
+import Switch from "react-router-dom"
+import Register from './components/Registration'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img src = "careLogo.png" alt="Christiana Care Logo" width="200"/>
+        <p class="top">Patient</p>
+        <Register/>
     </div>
   );
 }
